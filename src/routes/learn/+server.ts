b74de0b029b6be93c871568d970336d1e6d67268
mpoloch <martin.poloch@gmail.com>
2026-06-28 16:06:37 +0200

    feat: add /learn route serving deployment guide

---
+++ src/routes/learn/+server.ts
@@ -0,0 +1,10 @@
+import type { RequestHandler } from './$types';
+import htmlContent from '../../../docs/deployment-guide.html?raw';
+
+export const prerender = true;
+
+export const GET: RequestHandler = () => {
+	return new Response(htmlContent, {
+		headers: { 'Content-Type': 'text/html; charset=utf-8' },
+	});
+};
